1619ce83a9ef104b57f84c6e2379996665dc5614
abjj1999 <abdullahalhilfi21@gmail.com>
2021-11-14 18:03:24 -0600

    last touches

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,21 +4,21 @@ function Nav({ setContactSelected, setPortfoilioSelected, setHeaderSelected }) {
     return (
         <ul className="nav">
             <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#" onClick={function() {
+                <a className="nav-link" aria-current="page"  onClick={function() {
                     setHeaderSelected(true)
                     setContactSelected(false)
                     setPortfoilioSelected(false)
                 } }>About Me</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#" onClick={function() {
+                <a className="nav-link"  onClick={function() {
                     setHeaderSelected(false)
                     setContactSelected(false)
                     setPortfoilioSelected(true)
                 } }>Portfolio</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#" onClick={function() {
+                <a className="nav-link"  onClick={function() {
                     setHeaderSelected(false)
                     setContactSelected(true)
                     setPortfoilioSelected(false)
